b8e3a02e4fbd20d99aacc5bb0b27758138cf0ef2
bautigq <bgonzalezquesnel1@mail.austral.edu.ar>
2025-05-05 12:57:51 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,10 @@
-def math():
+ddef math():
     a = 57
     b = 7
+    print("suma:",a,"+",b,"=",a+b)
+    print("diferencia:",a,"-",b,"=",a-b)
+    print("producto:",a,"*",b,"=",a*b)
+    print("promedio: (",a,"+", b,") / 2 =", (a + b) / 2)
+    print("Cociente entero: ",a,"//", b,"=", (a//b))
+    print("Resto de la división entera: ",a,"%", b,"=", (a%b))
+    print("Valor real de la división: ",a,"/", b,"=", (a/b))
